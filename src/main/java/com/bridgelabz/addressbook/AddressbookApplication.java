916c452686648859d960d0e10d593a4aeaccecee
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-29 00:49:31 +0530

    added lombok library

--- src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -2,12 +2,16 @@ package com.bridgelabz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 public class AddressbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookApplication.class, args);
+		
 	}
 
 }
