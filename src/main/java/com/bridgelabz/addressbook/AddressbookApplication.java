0f44a2722d1296d66dd0633103fac66263e43065
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-29 00:53:11 +0530

    using lombok library for logging slf4j

--- src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -7,11 +7,13 @@ import org.springframework.context.ApplicationContext;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressbookApplication.class, args);
-		
+		ApplicationContext context=SpringApplication.run(AddressbookApplication.class, args);
+		log.info("AddressBook App started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
